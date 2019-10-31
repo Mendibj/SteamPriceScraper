85e61bc2af01ded71e92515862478041a472e91c
Mendibj <37113509+Mendibj@users.noreply.github.com>
2019-10-31 14:17:38 +0100

    Error logs
    Error logs and some fixes

--- ws.py
+++ ws.py
@@ -2,7 +2,6 @@ import requests
 import csv
 import os
 import pandas
-
 from bs4 import BeautifulSoup
 from datetime import datetime
 
@@ -20,10 +19,11 @@ names = []
 cPrices = []
 tPrices = []
 
-todayDate = datetime.today().strftime('%d-%m-%Y')
+todayDate = datetime.today().strftime('%d-%m-%Y %H:%M')
 contadorPaginas = 1
 
 strPage = "https://store.steampowered.com/search/?page="
+logError = open("Logs_error.txt","a",encoding='utf-8')
 
 page = requests.get(strPage)
 soup = BeautifulSoup(page.content, features="lxml")
@@ -33,47 +33,49 @@ lastPage = int(children[2].text)
 
 while(contadorPaginas <= lastPage):
     page = requests.get(strPage+str(contadorPaginas))
-    soup = BeautifulSoup(page.content, features="lxml")
-    resultsRows = soup.find(id="search_resultsRows")
+    resultsRows = None
+    intentos = 0
+
+    while(resultsRows == None and intentos < 10):
+        soup = BeautifulSoup(page.content, features="lxml")
+        resultsRows = soup.find(id="search_resultsRows")
+        intentos += 1
 
     if(resultsRows == None):
-        for i in range(0,10) and resultsRows == None:
-            page = requests.get(strPage+str(contadorPaginas))
-            soup = BeautifulSoup(page.content, features="lxml")
-            resultsRows = soup.find(id="search_resultsRows")
+        logError.write(datetime.now().strftime("%d/%m/%Y %H:%M:%S")+' Error al cargar la página '+str(contadorPaginas)+'\n')
+    else:
+        resultsA = resultsRows.findAll('a')
+        for a in resultsA:
+            try:
+                title = a.find('span', attrs={'class':'title'}).text
+                price = a.find('div', attrs={'class':'col search_price responsive_secondrow'})
+                cprice = 0
 
-        if(resultsRows == None):
-            continue
+                #El juego no está rebajado o es gratis
+                if(price != None):
+                    price = formatNumber(price.text.strip())
+                    cprice = price
+                #Juego rebajado
+                else:
+                    price = a.find('div', attrs={'class':'col search_price discounted responsive_secondrow'}).text.strip()
+                    pricesL = price[:-1].split("€")
+                    price = formatNumber(pricesL[0])
+                    cprice = formatNumber(pricesL[1])
 
-    resultsA = resultsRows.findAll('a')
-    for a in resultsA:
-        title = a.find('span', attrs={'class':'title'}).text
-        price = a.find('div', attrs={'class':'col search_price responsive_secondrow'})
-        cprice = 0
-
-        #El juego no está rebajado o es gratis
-        if(price != None):
-            price = formatNumber(price.text.strip())
-            cprice = price
-        #Juego rebajado
-        else:
-            price = a.find('div', attrs={'class':'col search_price discounted responsive_secondrow'}).text.strip()
-            pricesL = price[:-1].split("€")
-            price = formatNumber(pricesL[0])
-            cprice = formatNumber(pricesL[1])
-        
-        dates.append(todayDate)
-        names.append("\'"+title+"\'")
-        cPrices.append(cprice)
-        tPrices.append(price)
+                dates.append(todayDate)
+                names.append("\'"+title+"\'")
+                cPrices.append(cprice)
+                tPrices.append(price)
+            except:
+                logError.write(datetime.now().strftime("%d/%m/%Y %H:%M:%S")+' Error al cargar la página '+str(contadorPaginas)+'\n')
 
     print(str(contadorPaginas) + ' de '+str(lastPage))
     contadorPaginas += 1
     
 df = pandas.DataFrame(data={"Date": dates, "Name": names, "Total price": tPrices, "Current price": cPrices})
-#df.to_csv("./file.csv", sep=',',index=False)
 
 if not os.path.isfile("filename.csv"):
     df.to_csv("filename.csv", sep=',',index=False)
 else:
     df.to_csv("filename.csv", mode='a', header=False)
+logError.close()
